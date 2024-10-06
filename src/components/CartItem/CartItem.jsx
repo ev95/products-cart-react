8c6cec2ff147b9c29b3de71c56a15e0a26642d64
Yevgine <ev.shahinyan@gmail.com>
2024-10-07 01:33:48 +0400

    Added cart page functionality

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import style from './CartItem.module.css'
 
-const CartItem = ({cartItem}) => {
+const CartItem = ({ cartItem, increaseQuantity, decreaseQuantity, removeItem }) => {
+
   return (
-    <div>
-        <h4>{cartItem.title}</h4>
-        <h4>{cartItem.initprice}</h4>
+    <div className={style.cart_item} key={cartItem.id}>
+      <img src={cartItem.image} alt="Product Image" className={style.product_image} />
+      <div className={style.product_details}>
+        <h2 className={style.title}>{cartItem.title} </h2>
+        <p className={style.description}>{cartItem.description.slice(0, 100)}..</p>
+        <p> ${cartItem.price}</p>
+        <div className={style.quantity_container}>
+          <button className={style.quantity_btn} onClick={() => decreaseQuantity(cartItem)}>-</button>
+          <span className={style.quantity}>{cartItem.count}</span>
+          <button className={style.quantity_btn} onClick={() => increaseQuantity(cartItem)}>+</button>
+        </div>
+      </div>
+      <div className={style.product_price}>${cartItem.initprice}</div>
+
+      <div className={style.remove_item}>
+        <button onClick={() => removeItem(cartItem.id)}>Remove</button>
+      </div>
     </div>
   )
 }
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,18 +1,77 @@
-import React from 'react'
-import style from './CartPage.module.css'
+import React, { useState } from 'react'
 import CartItem from '../../components/CartItem/CartItem'
 
-const CartPage = ({cart}) => {
+import style from './CartPage.module.css'
+
+const CartPage = ({ cart }) => {
+  const [cartProducts, setCartProducts] = useState(cart);
+
+
+  function decreaseQuantity(product) {
+    if (product.count <= 1) return;
+
+    setCartProducts(cartProducts.map((elem) => {
+      if (elem.id === product.id) {
+        return {
+          ...elem,
+          count: --elem.count,
+          initprice: elem.count * elem.price
+        }
+      } else {
+        return elem
+      }
+    }))
+  }
+
+  function increaseQuantity(product) {
+
+    setCartProducts(cartProducts.map((elem) => {
+      if (elem.id === product.id) {
+        return {
+          ...elem,
+          count: ++elem.count,
+          initprice: elem.count * elem.price
+        }
+      } else {
+        return elem
+      }
+    }))
+  }
+
+  function removeItem(id) {
+    setCartProducts([
+      ...cartProducts.filter((p) => p.id !== id),
+    ]);
+  }
+
+  function totalPrice() {
+    let total = 0;
+    cartProducts.forEach(product => {
+      total += product.initprice
+    });
+    return total.toFixed(2);
+  }
 
   return (
-    <div>
+    <div className="container">
+      <h1>Shopping Cart</h1>
       {
-        cart.map((cartItem) => {
-          return <CartItem key={cartItem.id} cartItem={cartItem} />
+        cartProducts.map((cartItem) => {
+          return <CartItem key={cartItem.id}
+            cartItem={cartItem}
+            decreaseQuantity={decreaseQuantity}
+            increaseQuantity={increaseQuantity}
+            removeItem={removeItem} />
         })
       }
+
+      <div className={style.cart_total}>
+        <h3>Total: {totalPrice()}</h3>
+      </div>
     </div>
+
   )
+
 }
 
 export default CartPage
