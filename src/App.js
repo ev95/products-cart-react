03741c9b6493d7a00f9b9d240258060d133ea22f
Yevgine <ev.shahinyan@gmail.com>
2024-10-10 20:11:11 +0400

    Refactored code

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App({products}) {
 
   const [cart, setCart] = useState([]);
 
-  // addCartFunctional
+  // Add product to cart
   const addToCart = (item) => {
     let isContain = false;
 
@@ -38,13 +38,58 @@ function App({products}) {
     }
   }
 
+  // Decrease cart product cuantity
+  function decreaseQuantity(product) {
+    if (product.count <= 1) return;
+
+    setCart(cart.map((elem) => {
+      if (elem.id === product.id) {
+        return {
+          ...elem,
+          count: --elem.count,
+          initprice: elem.count * elem.price
+        }
+      } else {
+        return elem
+      }
+    }))
+  }
+
+    // Increment cart product cuantity
+  function increaseQuantity(product) {
+
+    setCart(cart.map((elem) => {
+      if (elem.id === product.id) {
+        return {
+          ...elem,
+          count: ++elem.count,
+          initprice: elem.count * elem.price
+        }
+      } else {
+        return elem
+      }
+    }))
+  }
+
+  // Remov product from cart
+  function removeItem(id) {
+    setCart([
+      ...cart.filter((p) => p.id !== id),
+    ]);
+  }
+
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Loyout cart={cart}/>}>
           <Route index path='/' element={<HomePage /> }/>
           <Route  path='/products' element={<ProductsPage products={products}  addToCart={ addToCart}/> }/>
-          <Route  path='/cart' element={<CartPage cart={cart}/>  }/>
+          <Route  path='/cart' element={<CartPage 
+          cart={cart}
+          decreaseQuantity={decreaseQuantity}
+          increaseQuantity={increaseQuantity}
+          removeItem={removeItem}
+          />  }/>
         </Route>
       </Routes>
     </div>
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,62 +1,20 @@
-import React, { useState } from 'react'
 import CartItem from '../../components/CartItem/CartItem'
 
 import style from './CartPage.module.css'
 
-const CartPage = ({ cart }) => {
-  const [cartProducts, setCartProducts] = useState(cart);
+
+const CartPage = ({ cart, removeItem, decreaseQuantity, increaseQuantity }) => {
+  let total = cart.reduce((accum, prod) => {
+    return accum + prod.initprice;
+  }, 0);
 
 
-  function decreaseQuantity(product) {
-    if (product.count <= 1) return;
-
-    setCartProducts(cartProducts.map((elem) => {
-      if (elem.id === product.id) {
-        return {
-          ...elem,
-          count: --elem.count,
-          initprice: elem.count * elem.price
-        }
-      } else {
-        return elem
-      }
-    }))
-  }
-
-  function increaseQuantity(product) {
-
-    setCartProducts(cartProducts.map((elem) => {
-      if (elem.id === product.id) {
-        return {
-          ...elem,
-          count: ++elem.count,
-          initprice: elem.count * elem.price
-        }
-      } else {
-        return elem
-      }
-    }))
-  }
-
-  function removeItem(id) {
-    setCartProducts([
-      ...cartProducts.filter((p) => p.id !== id),
-    ]);
-  }
-
-  function totalPrice() {
-    let total = 0;
-    cartProducts.forEach(product => {
-      total += product.initprice
-    });
-    return total.toFixed(2);
-  }
 
   return (
     <div className="container">
       <h1>Shopping Cart</h1>
       {
-        cartProducts.map((cartItem) => {
+        cart.map((cartItem) => {
           return <CartItem key={cartItem.id}
             cartItem={cartItem}
             decreaseQuantity={decreaseQuantity}
@@ -66,7 +24,7 @@ const CartPage = ({ cart }) => {
       }
 
       <div className={style.cart_total}>
-        <h3>Total: {totalPrice()}</h3>
+        <h3>Total: {total.toFixed(2)}</h3>
       </div>
     </div>
 
